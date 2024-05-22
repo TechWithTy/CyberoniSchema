5840f4a90d2585e8c8bdc4c4e434ff16b4671c0e
nehal7-ml <nehal.sk.99@gmail.com>
2024-05-23 04:06:28 +0530

    added subscription models, credits, subscriptions

--- scripts/script.ts
+++ scripts/script.ts
@@ -1,21 +1,29 @@
 import { prisma } from '../prismaClient'
-function generateCustomId() {
-    let id = '';
-    const characters = 'cdefghijklmnopqrstuvwxyz0123456789'; // Include 'c' and remove 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    const charactersLength = characters.length;
-    for (let i = 0; i < 24; i++) { // Reduce the loop iteration by 1 since we're adding 'c' manually
-        id += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return 'c' + id; // Add 'c' manually at the beginning
-}
+
+
+const categories = [
+    'Security Solutions',
+    'Networking Tools',
+    'Data Analysis',
+    'Cloud Services',
+    'Communication Tools',
+    'Project Management',
+    'Automation Software',
+    'Artificial Intelligence',
+    'E-commerce Platforms',
+    'Customer Relationship Management (CRM)'
+];
+
 async function runScript() {
-    const user = prisma.user;
+    categories.forEach(async name => {
+        await prisma.softwareProductCategory.create({
+            data: {
+                name
+            }
 
-    await user.update({ where: { email: 'cyberboss@cybershoptech.com' }, data: { id: generateCustomId() } })
-    await user.update({ where: { email: 'nehal.sk.99@gmail.com' }, data: { id: generateCustomId() } })
-
-    //await user.update({ where: { email: '' }, data: { id: generateUniqueId() } })
+        })
+    });
 
 }
 
-// runScript()
+runScript()
